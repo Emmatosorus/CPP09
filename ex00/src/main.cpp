e2780d93a3d0a4dea65819455fab7e782d7c51ff
Emmatosorus <emma@bouteyres.com>
2024-09-08 15:41:31 +0200

    Add ✨: All necessary files, first version of header file, some incomplete code in source file

---
+++ ex00/src/main.cpp
@@ -0,0 +1,17 @@
+#include "../includes/BitcoinExchange.h"
+#include <iostream>
+
+int main(int ac, char **av)
+{
+	try
+	{
+		if (ac != 2)
+			throw std::invalid_argument("Program must be given a filename as argument");
+
+
+	}
+	catch(std::exception & e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+}
